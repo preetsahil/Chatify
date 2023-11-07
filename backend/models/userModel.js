22ecaf3f8c69d0129d4381cb81457d8422710806
sahil <preetsahil289@gmail.com>
2023-11-08 01:55:50 +0530

    Added models for the application

---
+++ backend/models/userModel.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const userModel = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    pic: {
+      type: String,
+      required: true,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+const User = mongoose.model("User", userModel);
+
+module.exports = User;
