8d24d6667906102359fec03b2ee6562142e7172a
sahil <preetsahil289@gmail.com>
2023-11-14 21:09:20 +0530

    added authMiddleware and created chat API's

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
 app.use(express.json());
@@ -15,6 +17,8 @@ app.get("/", (req, res) => {
   res.send("API is running");
 });
 app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+
 app.use(notFound);
 app.use(errorHandler);
 const port = process.env.PORT;
